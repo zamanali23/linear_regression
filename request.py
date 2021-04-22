ca8df9c7a485b36dcd77d6fb0e7d3b8faf15955f
zaman ali <77948929+zamanali23@users.noreply.github.com>
2021-04-22 00:44:13 -0500

    Update request.py

--- request.py
+++ request.py
@@ -1,9 +1,9 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'alcohol':1, 'malic_acid':2, 'ash':3,'alcalinity_of_ash':4,'magnesium':5 ,
-'total_phenols':6 ,'flavanoids':7 ,'nonflavanoid_phenols':8 ,'proanthocyanins':9 ,'color_intensity':10,
-'hue':11,'od280/od315_of_diluted_wines':12, 'proline':13 })
+r = requests.post(url,json={'Date':1, 'Rented Bike Count':2, 'Hour':3,'Temparature(C)':4,'Humidity(%)':5 ,
+'Wind Speed(m/s)':6 ,'Visibilty(10m)':7 ,'Dew point temperature(C) ':8 ,'Solar Radiation (MJ/m2)':9 ,'Rainfall(mm)':10,
+'Snowfall(cm)':11,'Seasons':12, 'Holiday':13, 'Functioning Day':14  })
 
 print(r.json())
 
